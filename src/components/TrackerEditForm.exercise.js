b6cfa277af8c1b1e59ab5a99dfbe905c47fff6ae
Maria'lena Pietri <marialena.pietri@maps-system.com>
2021-12-19 23:18:22 +0100

    exo finished

--- src/components/TrackerEditForm.exercise.js
+++ src/components/TrackerEditForm.exercise.js
@@ -6,36 +6,58 @@ import {getDateTimeForPicker} from '../helper'
 // 🐶 Décommente la fontion "newTracker" qui te crée un objet tracker avec un
 // 'id' généré automatiquement et un 'starttime '
 
-// const newTracker = () => ({
-//   id: uuidv4(),
-//   category: 'Défault',
-//   starttime: getDateTimeForPicker(),
-//   endtime: '',
-//   name: '',
-// })
+const newTracker = () => ({
+  id: uuidv4(),
+  category: 'Défault',
+  starttime: getDateTimeForPicker(),
+  endtime: '',
+  name: '',
+})
 
 // 🐶 créé les 4 props 'selectedTracker', 'onAddTracker', 'onDeleteTracker' et 'onUpdateTracker'
 // initalise par défaut 🤖 `selectedTracker avec newTracker()` et surchage pour que l'id soit une string vide ""
-const TrackerEditForm = ({}) => {
+const TrackerEditForm = ({selectedTracker = {...newTracker(), id:''}, onAddTracker, onDeleteTracker, onUpdateTracker}) => {
   // 🐶 créé un state 'tracker' initialisé avec 'selectedTracker'
-
+const [tracker, setTracker] = React.useState(selectedTracker)
   // 🐶 les 4 fonctions qui suivent sont appelé sur un changement de valeur dans le formulaire
   // met à jour le state 'tracker' avec les nouvelle valeur du formulaire
-  const handleTrackerName = e => {}
-  const handleTrackerStartTime = e => {}
-  const handleTrackerEndTime = e => {}
-  const handleTrackerCategory = e => {}
+  const handleTrackerName = e => {
+    setTracker({...tracker, name: e.target.value})
+  }
+  const handleTrackerStartTime = e => {
+    setTracker({...tracker, starttime: e.target.value})
+  }
+  const handleTrackerEndTime = e => {
+    setTracker({...tracker, endtime: e.target.value})
+  }
+  const handleTrackerCategory = e => {
+    setTracker({...tracker, category: e.target.value})
+  }
 
   // 🐶 créé une fonction 'handleOnSubmit' qui va appeler 'onAddTracker'
   // ps : n'oublie pas le  e.preventDefault()
+  const handleOnSubmit = e => {
+    e.preventDefault()
+    onAddTracker(tracker)
+  }
 
   // 🐶 créé une fonction 'handleUpdateTracker' qui va appeler 'onUpdateTracker'
-
+  const handleUpdateTracker = e => {
+    e.preventDefault()
+    onUpdateTracker(tracker)
+  }
   // 🐶 créé une fonction 'handleDeleteTracker' qui va appeler 'onDeleteTracker'
-
+  const handleDeleteTracker = e => {
+    e.preventDefault()
+    onDeleteTracker(tracker)
+  }
   // 🐶 créé une fonction 'handleNewTracker' qui va mettre à jour le state tracker
   // avec newTracker()
+  const handleNewTracker = e => {
+    e.preventDefault()
+    setTracker(newTracker)
 
+  }
   // 🐶 met à jour le state tracker quand 'selectedTracker' change de valeur.
   // ceci ce produit lors d'un clique sur le tableau par exemple, une nouvelle
   // valeur de 'selectedTracker' arrive et il faut mettre à jour le state.
@@ -43,31 +65,46 @@ const TrackerEditForm = ({}) => {
   // conditionne la mise à jour du tracker si les ids sont differents et non vide
   // 🤖 selectedTracker?.id !== '' && selectedTracker?.id !== tracker.id
 
+  React.useEffect(() => {
+    if(selectedTracker?.id !== '' && selectedTracker?.id !== tracker.id ) {
+      setTracker(selectedTracker)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[selectedTracker])
+
   // 🐶 On veut maintenant activer / desactiver les boutons / Champs input en fonction
   // de l'état du tracker (pas de tracker à editer / tracker à editer )
   // on se base sur l'id
   // 🤖 créée const disabled
   // si id vide 'disabled' est à true, false sinon
   // met `disabled={disabled}` sur tous les champs <input< et <button> (sauf le boutton 'Nouveau Tracker')
-
+const disabled = tracker.id === ''
   return (
     <>
-      {/* 🐶 defini 'handleOnSubmit' sur l'event 'onSubmit' du formulaire */}
-      <form className="Form">
+
+      <form className="Form" onSubmit={handleOnSubmit}>
         <fieldset>
           <legend>Gestion des Trackers</legend>
-          {/* 🐶 créé un 'label' et 'input' type 'text' pour le tracker.name */}
-          {/* 🐶 créé un 'label' et 'input' type 'datetime-local' pour le tracker.starttime */}
-          {/* 🐶 créé un 'label' et 'input' type 'datetime-local' pour le tracker.endtime */}
-          {/* 🐶 créé un 'label' et 'select' 'option' pour le tracker.category */}
+          <label htmlFor="name">Nom du tracker :
+            <input type="text" name="name" value={tracker.name} disabled={disabled} onChange={handleTrackerName}/>
+          </label>
+          <label htmlFor="starttime">
+            <input type="datetime-local" name="starttime" value={tracker.starttime} disabled={disabled} onChange={handleTrackerStartTime}/>
+          </label>
+          <label htmlFor="endtime">
+            <input type="datetime-local" name="endtime" value={tracker.endtime} disabled={disabled} onChange={handleTrackerEndTime}/>
+          </label>
+          <label htmlFor="category">
+            <input type="select" name="category" value={tracker.category} disabled={disabled} onChange={handleTrackerCategory}/>
+                      </label>
 
           <label>Actions</label>
           <div className="Action">
-            {/* 🐶 créé button 'Nouveau Tracker' avec 'onClick' = 'handleNewTracker' */}
-            {/* 🐶 créé button 'Ajouter' qui permet de soumettre le formulaire */}
-            {/* 🤖 utilise input type="submit"  */}
-            {/* 🐶 créé button 'Supprimer' avec 'onClick' = 'handleDeleteTracker' */}
-            {/* 🐶 créé button 'Mettre à jour' avec 'onClick' = 'handleUpdateTracker' */}
+            <input type="button" onClick={handleNewTracker} value="Nouveau Tracker"/>
+            <input type="submit" value="Ajouter" disabled={disabled}/>
+            <input type="button" onClick={handleDeleteTracker} value="Supprimerr" disabled={disabled}/>
+            <input type="button" onClick={handleUpdateTracker} value="Mettre à jourr" disabled={disabled}/>
+
           </div>
         </fieldset>
       </form>
--- src/components/TrackersApp.exercise.js
+++ src/components/TrackersApp.exercise.js
@@ -3,6 +3,7 @@ import * as React from 'react'
 import {FilterTrackers} from './FilterTrackers'
 import {TrackersTable} from './TrackersTable'
 // 🐶 importe TrackerEditForm
+import {TrackerEditForm} from './TrackerEditForm'
 import db from '../data'
 
 function TrackersApp() {
@@ -22,21 +23,57 @@ function TrackersApp() {
   // et qui ajoute ce 'tracker' dans 'allTrackers' avec 'setAllTrackers'
   // tu peux utiliser un spread opérator (ou solution équivalente) :
   // 🤖 [...arrayExistant, nouvelElement]
+    const handleAddTracker = (tracker) => {
+        if (!tracker.id) {
+            alert ('il manque le tracker id')
+        } else if(!tracker.name) {
+            alert('veuillez renseigner le nom du tracker')
+        } else if (!tracker.starttime) {
+            alert('veuillez renseigner la date de début')
+        } else if(!tracker.category)  {
+            alert( 'veuillez renseigner la catégori')
+        }else {
+            setAllTrackers([...allTrackers, tracker])
+        }
+    }
 
   // 🐶 créé une fonctions 'handleDeleteTracker' qui prend en paramètre un 'tracker'
   // et qui supprime ce 'tracker' de 'allTrackers' en se basant dur l'id
   // utilise 'filter' (ou solution équivalente) :
   // 🤖 arrayExistant.filter((item) => item.id !== elementASupprimer.id)
+const handleDeleteTracker = (deletedTracker) => {
+    if (!deletedTracker.id) {
+        alert ('il manque le tracker id')
+    } else {
+        const filteredTrackers = allTrackers.filter(tracker => tracker.id !== deletedTracker.id)
+        setAllTrackers(filteredTrackers)
+    }
+}
 
   // 🐶 créé une fonctions 'handleUpdateTracker' qui prend en paramètre un 'tracker'
   // et qui met à jour ce 'tracker' dans 'allTrackers' en se basant dur l'id
   // utilise 'map' (ou solution équivalente):
   // 🤖 updatedArray = arrayExistant.map(item => item.id === elementAMaj.id ? elementAMaj : item)
+    const handleUpdateTracker = (updateTracker) => {
+        if (!updateTracker.id) {
+            alert ('il manque le tracker id')
+        } else if(!updateTracker.name) {
+            alert('veuillez renseigner le nom du tracker')
+        } else if (!updateTracker.starttime) {
+            alert('veuillez renseigner la date de début')
+        } else if(!updateTracker.category)  {
+            alert( 'veuillez renseigner la catégori')
+        }else {
+            const updatedTrackers = allTrackers.map(tracker => tracker.id === updateTracker.id ? updateTracker : tracker)
+            setAllTrackers(updatedTrackers)
+        }
+    }
 
   return (
     <div>
       <FilterTrackers onTextChange={handleTextChange} />
       {/* 🐶 appelle <TrackerEditForm> avec les 4 props utiles */}
+        <TrackerEditForm selectedTracker={selectedTracker} onAddTracker={handleAddTracker} onDeleteTracker={handleDeleteTracker} onUpdateTracker={handleUpdateTracker}/>
       <TrackersTable
         trackers={allTrackers}
         selectedTracker={selectedTracker}
